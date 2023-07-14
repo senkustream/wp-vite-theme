c5eddb8707284b8c7d81f962cde5d882249c2b96
Kresna Satya <kresnasatya72@gmail.com>
2023-07-14 18:50:05 +0800

    add theme support menus

--- functions.php
+++ functions.php
@@ -15,3 +15,15 @@ $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 require_once 'inc/vite.config.php';
+
+if (! function_exists('wpvite_theme_support')) {
+    function wpvite_theme_support() : void {
+        add_theme_support( 'menus' );
+
+        register_nav_menus(array(
+            'header-nav' => 'Header Navigation'
+        ));
+    }
+}
+
+add_action( 'after_setup_theme', 'wpvite_theme_support' );
